91bce51c59baefba6152a58d2ee61e66c7dc9ceb
yclanlan <lanlan.yichun@gmail.com>
2023-08-11 16:42:28 -0400

    publicDir: '../public',

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in proc
 
 export default {
     root: 'src/',
-    publicDir: '',
+    publicDir: '../public',
     base: './blender--threejs-practice',
     server:
     {
